05173cfefbb38a179f146230ab49afbd2f27622e
zhouxupython <james19840406@163.com>
2018-12-06 23:57:58 +0800

    011

---
+++ upload2.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.011 ./
+git add .
+git commit -m "011"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.012 ./
+git add .
+git commit -m "012"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.013 ./
+git add .
+git commit -m "013"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.014 ./
+git add .
+git commit -m "014"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.015 ./
+git add .
+git commit -m "015"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.016 ./
+git add .
+git commit -m "016"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.017 ./
+git add .
+git commit -m "017"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.018 ./
+git add .
+git commit -m "018"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.019 ./
+git add .
+git commit -m "019"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.020 ./
+git add .
+git commit -m "020"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.021 ./
+git add .
+git commit -m "021"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.022 ./
+git add .
+git commit -m "022"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+mv ~/resource/experiment-tools/mooc-os-2015.vdi.bz2.023 ./
+git add .
+git commit -m "023"
+git push
+
+echo "--------009 over-------"
+
+echo "--------all over-------"
